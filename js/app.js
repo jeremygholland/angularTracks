a330271378afd250b9592b68f1bcfdcfda0a2e7d
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-06-22 23:26:19 -0500

    corrected targetting of district search

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ var app = angular.module('app', []);
 
 app.controller('mainCtrl', ['$scope', function($scope){
 	$scope.test = 'test';
+	$scope.hidden = false;
 
 	$scope.search = function(){
-		var dicks = [];
+		$scope.hidden = true;
 		$('.name').html('');
 		console.log($scope.zip);
 		$.getJSON("http://congress.api.sunlightfoundation.com/legislators/locate?zip="+$scope.zip+"&apikey=8b48c930d6bb4552be3b0e6248efb463").then(function (json){
@@ -15,27 +16,25 @@ app.controller('mainCtrl', ['$scope', function($scope){
 					var lastName = json.results[i].last_name;
 					var state = json.results[i].state;
 				$('.name').append("<td> <button id = "+district+"> "+district+"</button> </td> <td> <h4>"+firstName+" "+lastName+ "</h4> </td>");
-				$('#'+district).click(function(){
+				$('#'+district).click(function(event){
 					$('.name').html('');
-					var newSearch = $(this).text();
-					console.log(newSearch);
-					$.getJSON('http://congress.api.sunlightfoundation.com/legislators?state='+state+'&district='+district+'&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
+					var newSearch = (event.target.id);
+					$.getJSON('http://congress.api.sunlightfoundation.com/legislators?state='+state+'&district='+newSearch+'&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
 						var firsties = json.results[0].first_name;
 						var lasties = json.results[0].last_name;
 						var party = json.results[0].party;
 						var id = json.results[0].bioguide_id;
-						$('.name').html("<h2> " + firsties +" "+ lasties + "("+party+")</h2>");
+						var contact = json.results[0].contact_form;
+						$('.name').html("<h2> <a href ="+contact+">" + firsties +" "+ lasties + "("+party+")</a></h2>");
 						$.getJSON('http://congress.api.sunlightfoundation.com/bills?sponsor_id='+id +'&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
 							for(j=0; j<json.results.length; j++){
+								if(json.results[j].congress == '114'){
 								var bills = json.results[j].bill_id;
-								var committee = json.results[j].committee_ids
-								$('.bills').append('<p>' + bills + '</p>');
-								$.getJSON('http://congress.api.sunlightfoundation.com/committees?committee_id='+committee+'&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
-									for(b = 0; b<json.results.length; b++){
-										var committeeName= json.results[b].name;
-										$('.committees').append('<p>' + committeeName + '</p>');
-									}
-								})
+								var committee = json.results[j].committee_ids;
+								var shortTitle = json.results[j].short_title;
+								var link = json.results[j].last_version.urls.pdf;
+								$('.bills').append('<li>' + bills + ' <ul> <a href = '+ link +'> '+ shortTitle+'</a></ul></li>');
+								}
 							}
 						})
 					})
