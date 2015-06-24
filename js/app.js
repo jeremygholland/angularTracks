02e7fdb028457a86706422798b13b2aac15fbe4e
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-06-24 00:10:26 -0500

    added committees of legilsators and state of each legislature

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ app.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout){
 	$scope.test = 'test';
 	$scope.hidden = false;
 	$('.money').hide();
+	$('.comitStuff').hide();
 
 	$scope.search = function(){
 		$scope.hidden = true;
@@ -24,6 +25,7 @@ app.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout){
 				$('.name').append("<td> <button id = "+district+"> "+district+"</button> </td> <td> <h4>"+firstName+" "+lastName+ "</h4> </td>");
 				$('#'+district).click(function(event){
 					$('.name').html('');
+					$('.comitStuff').show();
 					var newSearch = (event.target.id);
 					$.getJSON('http://congress.api.sunlightfoundation.com/legislators?state='+state+'&district='+newSearch+'&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
 						var firsties = json.results[0].first_name;
@@ -32,16 +34,21 @@ app.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout){
 						var id = json.results[0].bioguide_id;
 						var contact = json.results[0].contact_form;
 						console.log(id);
-						$('.name').html("<h2> <a href ="+contact+">" + firsties +" "+ lasties + "("+party+")</a></h2>");
+						$('.name').html("<h2> <a href ="+contact+">" + firsties +" "+ lasties + "("+party+"), "+state+"</a></h2>");
 						$.getJSON('http://congress.api.sunlightfoundation.com/bills?sponsor_id='+id +'&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
 							for(j=0; j<json.results.length; j++){
 								if(json.results[j].congress == '114'){
-								var bills = json.results[j].bill_id;
-								var committee = json.results[j].committee_ids;
+								var bills = json.results[j].bill_id; 
 								var shortTitle = json.results[j].short_title;
 								var link = json.results[j].last_version.urls.pdf;
 								$('.bills').append('<li>' + bills + ' <ul> <a href = '+ link +'> '+ shortTitle+'</a></ul></li>');
 								}
+							}
+						})
+						$.getJSON('http://congress.api.sunlightfoundation.com/committees?member_ids='+id+'&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
+							for(w = 0; w<json.results.length; w++){
+									var committees= json.results[w].name;
+								$('.mainCommittee').append('<li>'+committees+'</li>');
 							}
 						})
 						//&callback=?
@@ -49,17 +56,25 @@ app.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout){
 							$('.money').show();
 							var newID = json[0].id;
 							console.log(newID);
-							var total = json[0].total_received;
-							$scope.total = total;
+							var totalNew = json[0].total_received;
+							$scope.total = totalNew;
 							var totalP = json[0].count_received;
 							console.log($scope.total);
 							$('.totalP').html(totalP);
+							//allows for angular currency filter to work
 							$timeout(function() { 
 								total.push(json[0].total_received);
 							}, 2000);
-							})
+						$.getJSON('http://transparencydata.com/api/1.0/aggregates/pol/'+newID+'/contributors.json?cycle=2012&limit=10&callback=?&apikey=8b48c930d6bb4552be3b0e6248efb463').then(function (json){
+							for(y = 0; y<json.length; y++){
+								var contribName = json[y].name;
+								console.log(contribName);
+								var contribTotal = json[y].total_amount;
+								$('.topContributors').append('<ul>'+contribName+'<li>'+contribTotal+'</li> </ul>');
+							}
+						})
 					})
-
+					})
 				})
 
 			}
